d6e0bb3badb2841e73cdb81a09e4e436f94f27dc
MartinScript <martinsojoola2@gmail.com>
2024-06-03 23:18:10 +0100

    fix bugs

--- 0x15-api/2-export_to_JSON.py
+++ 0x15-api/2-export_to_JSON.py
@@ -1,51 +1,33 @@
 #!/usr/bin/python3
-"""Using what you did in the task #0, extend your Python script to export data
-in the JSON format."""
-
+""" Python to get data from an API and convert to Json"""
+import csv
 import json
 import requests
 import sys
 
-base_url = "https://jsonplaceholder.typicode.com/"
-
-
-def do_request():
-    """request"""
-
-    if len(sys.argv) < 2:
-        return print("USAGE:", __file__, "<employee id>")
-    eid = sys.argv[1]
-    try:
-        _eid = int(sys.argv[1])
-    except ValueError:
-        return print("Employee id must be an integer")
-
-    response = requests.get(base_url + "users/" + eid)
-    if response.status_code == 404:
-        return print("User id not found")
-    elif response.status_code != 200:
-        return print("Error: status_code:", response.status_code)
-    user = response.json()
-
-    response = requests.get(base_url + "todos/")
-    if response.status_code != 200:
-        return print("Error: status_code:", response.status_code)
-    todos = response.json()
-    user_todos = [todo for todo in todos if todo.get("userId") == user.get("id")]
-    completed = [todo for todo in user_todos if todo.get("completed")]
-
-    user_todos = [
-        {
-            "task": todo.get("title"),
-            "completed": todo.get("completed"),
-            "username": user.get("username"),
-        }
-        for todo in user_todos
-    ]
-    data = {eid: user_todos}
-    with open(eid + ".json", "w") as file:
-        json.dump(data, file)
-
 
 if __name__ == "__main__":
-    do_request()
+    USER_ID = sys.argv[1]
+    url_to_user = "https://jsonplaceholder.typicode.com/users/" + USER_ID
+    res = requests.get(url_to_user)
+    """Documentation"""
+    USERNAME = res.json().get("username")
+    """Documentation"""
+    url_to_task = url_to_user + "/todos"
+    res = requests.get(url_to_task)
+    tasks = res.json()
+
+    dict_data = {USER_ID: []}
+    for task in tasks:
+        TASK_COMPLETED_STATUS = task.get("completed")
+        TASK_TITLE = task.get("title")
+        dict_data[USER_ID].append(
+            {
+                "task": TASK_TITLE,
+                "completed": TASK_COMPLETED_STATUS,
+                "username": USERNAME,
+            }
+        )
+    """print(dict_data)"""
+    with open("{}.json".format(USER_ID), "w") as f:
+        json.dump(dict_data, f)
