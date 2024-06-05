a85f73a36fb4e60d9933e0973c76cc6803ac082e
MartinScript <martinsojoola2@gmail.com>
2024-06-05 10:48:25 +0100

    corrections

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -1,28 +1,35 @@
 #!/usr/bin/python3
 """
-gather employee data from API
+Write a script that hits a REST api that has data on employees and tasks
+and filters the data based on an argument passed to the script.
+The argument is the employee ID, and the output should display the employee
+name and the tasks the employee completed.
 """
-
-import re
 import requests
 import sys
 
-REST_API = "https://jsonplaceholder.typicode.com"
-
 if __name__ == "__main__":
-    if len(sys.argv) > 1:
-        if re.fullmatch(r"\d+", sys.argv[1]):
-            id = int(sys.argv[1])
-            req = requests.get("{}/users/{}".format(REST_API, id)).json()
-            task_req = requests.get("{}/todos".format(REST_API)).json()
-            emp_name = req.get("name")
-            tasks = list(filter(lambda x: x.get("userId") == id, task_req))
-            completed_tasks = list(filter(lambda x: x.get("completed"), tasks))
-            print(
-                "Employee {} is done with tasks({}/{}):".format(
-                    emp_name, len(completed_tasks), len(tasks)
-                )
-            )
-            if len(completed_tasks) > 0:
-                for task in completed_tasks:
-                    print("\t {}".format(task.get("title")))
+    eid = sys.argv[1]
+    name = (
+        requests.get("http://jsonplaceholder.typicode.com/users/{}".format(eid))
+        .json()
+        .get("name")
+    )
+    total_tasks = 0
+    done_tasks = []
+    r = requests.get("http://jsonplaceholder.typicode.com/todos").json()
+
+    for task in r:
+        if task.get("userId") == int(eid):
+            total_tasks += 1
+            if task.get("completed"):
+                done_tasks.append(task.get("title"))
+
+    print(
+        "Employee {} is done with tasks({:d}/{:d}):".format(
+            name, len(done_tasks), total_tasks
+        )
+    )
+
+    for item in done_tasks:
+        print("\t {}".format(item))
